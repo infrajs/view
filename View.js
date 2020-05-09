7ac3d6a8d20176cb7d5a9dde7904a51678386c37
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-09 04:48:42 +0400

    fix

--- View.js
+++ View.js
@@ -1,89 +1,90 @@
-window.View = {
-	getPath:function(){//depricated плохо связано с такойже функцией на сервере
+import { Access } from '/vendor/infrajs/access/Access.js'
+let View = {
+	getPath: function () {//depricated плохо связано с такойже функцией на сервере
 		return location.pathname;
 	},
-	getRoot:function(){
+	getRoot: function () {
 		//Путь начинается без слэша svn/project/ например
-		var p=this.getPath();
-		p=p.split('/');
+		var p = this.getPath();
+		p = p.split('/');
 		p.pop();
-		if(!p[0])p.shift();
-		p=p.join('/');
-		if(p)p+='/';
+		if (!p[0]) p.shift();
+		p = p.join('/');
+		if (p) p += '/';
 		return p;
 	},
-	getAGENT:function(){
+	getAGENT: function () {
 		return navigator.userAgent;
 	},
-	getREF:function(){
-		if(document.http_referrer)return document.http_referrer;
+	getREF: function () {
+		if (document.http_referrer) return document.http_referrer;
 		return document.referrer
 	},
-	getREQUEST:function(name){
-		if(this.REQUEST)return this.REQUEST;
-		var REQUEST={};
+	getREQUEST: function (name) {
+		if (this.REQUEST) return this.REQUEST;
+		var REQUEST = {};
 		/*var DATA=this.getPOST();
 		for(var i in DATA)REQUEST[i]=DATA[i];*/
-		var DATA=this.getGET();
-		for(var i in DATA)REQUEST[i]=DATA[i];
-		this.REQUEST=REQUEST;
+		var DATA = this.getGET();
+		for (var i in DATA) REQUEST[i] = DATA[i];
+		this.REQUEST = REQUEST;
 		return REQUEST;
 	},
-	getCookie:function(name){
+	getCookie: function (name) {
 		//if(!this.cookies){ асинхронно установленные кукисы сервером, должны обнаруживаться, для этого разбираем куки каждый раз
-			this.cookies={};
-			infra.forr(document.cookie.split(';'),function( cookie ) {
-				var parts = cookie.split('=');
-				var key=parts[0]||'';
-				key=key.replace(/^\s+/,'');
-				key=key.replace(/\s+$/,'');
-				var val=parts[1]||'';
-				val=val.replace(/^\s+/,'');
-				val=val.replace(/\s+$/,'');
-				this.cookies[key]=val;
-			}.bind(this));
+		this.cookies = {};
+		infra.forr(document.cookie.split(';'), function (cookie) {
+			var parts = cookie.split('=');
+			var key = parts[0] || '';
+			key = key.replace(/^\s+/, '');
+			key = key.replace(/\s+$/, '');
+			var val = parts[1] || '';
+			val = val.replace(/^\s+/, '');
+			val = val.replace(/\s+$/, '');
+			this.cookies[key] = val;
+		}.bind(this));
 		//}
-		if(name)return this.cookies[name];
+		if (name) return this.cookies[name];
 		return this.cookies;
 	},
-	setCookie:function(name,val){
-		if(val===undefined)val='';
+	setCookie: function (name, val) {
+		if (val === undefined) val = '';
 		this.getCookie();
-		
-		var longdate=new Date();
+
+		var longdate = new Date();
 		longdate.setFullYear(2020);
-		
-		
-		if(val===''){
-			var deldate=new Date();
+
+
+		if (val === '') {
+			var deldate = new Date();
 			deldate.setFullYear(2000);
-			var expire=deldate;
+			var expire = deldate;
 			delete this.cookies[name];
-		}else{
-			val=String(val);
-			var expire=longdate;
-			this.cookies[name]=val;
+		} else {
+			val = String(val);
+			var expire = longdate;
+			this.cookies[name] = val;
 		}
 		//var httproot=infra.plugin.getHTTPROOT();//Куки для домена который сейчас в адресной строке
 		//var root=httproot?httproot.siteroot:'/';
 		//var root=infra.view.getRoot();
 		var root = '/';
 
-		var val=name + "=" + escape(val) + '; path='+root+'; expires=' + expire.toGMTString();
+		var val = name + "=" + escape(val) + '; path=' + root + '; expires=' + expire.toGMTString();
 		document.cookie = val;
 		return true;
 	},
-	setCOOKIE:function(name,val){
-		return this.setCookie(name,val);
+	setCOOKIE: function (name, val) {
+		return this.setCookie(name, val);
 	},
-	getCOOKIE:function(name){
+	getCOOKIE: function (name) {
 		return this.getCookie(name);
 	},
-	getQuery:function(){
-		var url=location.search;
+	getQuery: function () {
+		var url = location.search;
 		//url=decodeURIComponent(url);
 		//try {
-			url=decodeURI(url);
+		url = decodeURI(url);
 		//}catch(e){
 
 		//}
@@ -93,33 +94,33 @@ window.View = {
 		return m.join('?');*/
 		return url;
 	},
-	getHost:function(){
+	getHost: function () {
 		return location.host;
 	},
-	getGET:function(){
-		if(this.GET)return this.GET;
-		var query=this.getQuery();
-		var query=decodeURI(query);
-		query=query.replace(/^\?/,"");
-		var pars=query.split('&');
-		var GET={};
-		for(var i=0,l=pars.length;i<l;i++){
-			var par=pars[i];
-			if(!par)continue;
-			par=par.split('=');
-			var name=par[0];
-			GET[name]=par[1];
-			if(GET[name]&&Number(GET[name])==String(GET[name])){
-				GET[name]=Number(GET[name]);
+	getGET: function () {
+		if (this.GET) return this.GET;
+		var query = this.getQuery();
+		var query = decodeURI(query);
+		query = query.replace(/^\?/, "");
+		var pars = query.split('&');
+		var GET = {};
+		for (var i = 0, l = pars.length; i < l; i++) {
+			var par = pars[i];
+			if (!par) continue;
+			par = par.split('=');
+			var name = par[0];
+			GET[name] = par[1];
+			if (GET[name] && Number(GET[name]) == String(GET[name])) {
+				GET[name] = Number(GET[name]);
 			}
 		}
-		return this.GET=GET;
+		return this.GET = GET;
 	},
-	setTitle:function(title){
-		document.title=title;
+	setTitle: function (title) {
+		document.title = title;
 	}
 }
-infra.view = View;
+
 
 /*
  * Вставить элемент с id строку str всё это в сроке html
@@ -132,87 +133,88 @@ infra.view = View;
  * Второй вставить в id
  * id true  заменить весь html
  **/
- 
 
-infra.htmlclear=function(id){
-	var el=document.getElementById(id);
-	if(!el)return;
-	el.innerHTML='';
-	el.style.display='none';
+if (!window.infra) window.infra = {}
+
+infra.htmlclear = function (id) {
+	var el = document.getElementById(id);
+	if (!el) return;
+	el.innerHTML = '';
+	el.style.display = 'none';
 }
-infra.html=function(html,id){
-	if(!arguments.length)return document.body.innerHTML;
+infra.html = function (html, id) {
+	if (!arguments.length) return document.body.innerHTML;
 
-	this.html.scriptautoexec=false;
-	this.html.styleautoexec=false;
-	var tempid='jslibhtml'+infra.htmlGetUnick();//Одинаковый id нельзя.. если будут вложенные вызовы будет ошибка
+	this.html.scriptautoexec = false;
+	this.html.styleautoexec = false;
+	var tempid = 'jslibhtml' + infra.htmlGetUnick();//Одинаковый id нельзя.. если будут вложенные вызовы будет ошибка
 
-	html='<span id="'+tempid+'" style="display:none">'+
-	'<style>#'+tempid+'{ width:3px }</style>'+
-	'<script type="text/javascript">infra.html.scriptautoexec=true;</script>'+
-	'1</span>'+html;
+	html = '<span id="' + tempid + '" style="display:none">' +
+		'<style>#' + tempid + '{ width:3px }</style>' +
+		'<script type="text/javascript">infra.html.scriptautoexec=true;</script>' +
+		'1</span>' + html;
 
-	if(arguments.length==1){
-		var el=document.body;
-	}else if(id===true){
-		var el=document.body;
-	}else if(typeof(id)=='object'){
-		var el=id;
-	}else{
-		var el=document.getElementById(id);
+	if (arguments.length == 1) {
+		var el = document.body;
+	} else if (id === true) {
+		var el = document.body;
+	} else if (typeof (id) == 'object') {
+		var el = id;
+	} else {
+		var el = document.getElementById(id);
 	}
-	if(!el){
+	if (!el) {
 		console.log('Не найден div id');
 		return;
 	}
-	try{
-		var res=(el.innerHTML=html);
-		el.style.display='';
-	}catch(e){
-		el.innerHTML='Ошибка, Возможно из-за вставки блочного элемента в строчный или другое какое-то нелогичное действие';
+	try {
+		var res = (el.innerHTML = html);
+		el.style.display = '';
+	} catch (e) {
+		el.innerHTML = 'Ошибка, Возможно из-за вставки блочного элемента в строчный или другое какое-то нелогичное действие';
 	}
 
-	if(!this.html.scriptautoexec){
+	if (!this.html.scriptautoexec) {
 
 		var scripts = el.getElementsByTagName("script");
 		//for (var i = 0,script; script = scripts[i]; i++){
 		//подмена script через document.write или innerHTML изменяет и этот массив scripts
-		for (var i=0,l=scripts.length;i<l;i++){
-			(function(){
-				var script=scripts[i];
+		for (var i = 0, l = scripts.length; i < l; i++) {
+			(function () {
+				var script = scripts[i];
 				//setTimeout(function(){
-					   infra.htmlexec(script);
+				infra.htmlexec(script);
 				//},1);
 			})()
 		}
 	}
 
-	var bug=document.getElementById(tempid);
-	if(bug){
-		var b=infra.htmlGetStyle(bug,'width');
-		if(b!=='3px'){
-			var csss= el.getElementsByTagName("style");
-			for (var i = 0,css;css=csss[i];i++){
-				var t=css.cssText;//||css.innerHTML; для IE будет Undefined ну и бог с ним у него и так работает а сюда по ошибке поподаем
+	var bug = document.getElementById(tempid);
+	if (bug) {
+		var b = infra.htmlGetStyle(bug, 'width');
+		if (b !== '3px') {
+			var csss = el.getElementsByTagName("style");
+			for (var i = 0, css; css = csss[i]; i++) {
+				var t = css.cssText;//||css.innerHTML; для IE будет Undefined ну и бог с ним у него и так работает а сюда по ошибке поподаем
 
 
-				var style=document.createElement('style');
+				var style = document.createElement('style');
 				//style.innerHTML='@import url("'+href+'")';
-				style.innerHTML=t;
+				style.innerHTML = t;
 				document.getElementsByTagName('head')[0].appendChild(style);
 				//infra.style(t);
 			}
 		}
-		try{
+		try {
 			el.removeChild(bug);
-		}catch(e){
-			if(infra.debug())alert('Ошибка при удалении временного элемента в infra.html\n'+ e);
+		} catch (e) {
+			if (Access.debug()) alert('Ошибка при удалении временного элемента в infra.html\n' + e);
 		}
 	}
 	return res;
 }
-infra.htmlexec=function(script){
-	if(!script)return;
+infra.htmlexec = function (script) {
+	if (!script) return;
 	//if(htmlexec.busy){
 	//	setTimeout(function(){
 	//		infra.htmlexec(script);
@@ -220,28 +222,28 @@ infra.htmlexec=function(script){
 	//	return;
 	//}
 	//htmlexec.busy=true;
-	if(script.src){
+	if (script.src) {
 
 		//stencill
 		//(function() { 
-			//var src='http://counter.rambler.ru/top100.jcn?{config.id}';
-			var src=script.src;
-			var ga = document.createElement('script'); 
-			ga.type = script.type; 
-			ga.async = script.async;
-			ga.defer = script.defer;
-			ga.src = src;
-			var s = document.getElementsByTagName('script')[0]; 
-			s.parentNode.insertBefore(ga, s); 
+		//var src='http://counter.rambler.ru/top100.jcn?{config.id}';
+		var src = script.src;
+		var ga = document.createElement('script');
+		ga.type = script.type;
+		ga.async = script.async;
+		ga.defer = script.defer;
+		ga.src = src;
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
 		//})(); 
 		//htmlexec.busy=false;
 
 		/* infra.load(script.src,'ea',function(){
 			htmlexec.busy=false;
 		});*/
-	}else{ 
+	} else {
 		//try{
-			infra.htmlGlobalEval(script.innerHTML, script.type); 
+		infra.htmlGlobalEval(script.innerHTML, script.type);
 		//}catch(e){
 		//	var conf=infra.config();
 		//	if(conf.debug){
@@ -251,18 +253,18 @@ infra.htmlexec=function(script){
 		//htmlexec.busy=false;
 	}
 }
-infra.htmlGlobalEval=function(data, type) {
+infra.htmlGlobalEval = function (data, type) {
 	if (!data) return;
 
 	var head = document.getElementsByTagName("head")[0] || document.documentElement, script = document.createElement("script");
 	script.type = type;
 	script.text = data;
-	head.insertBefore( script, head.firstChild );
-	head.removeChild( script );
-	
+	head.insertBefore(script, head.firstChild);
+	head.removeChild(script);
+
 }
 
-infra.htmlGetStyle=function(el, cssprop){
+infra.htmlGetStyle = function (el, cssprop) {
 	if (el.currentStyle) //IE
 		return el.currentStyle[cssprop]
 	else if (document.defaultView && document.defaultView.getComputedStyle) //Firefox
@@ -270,32 +272,16 @@ infra.htmlGetStyle=function(el, cssprop){
 	else //try and get inline style
 		return el.style[cssprop]
 };
-infra.htmlGetUnick=function(){
-	var t=new Date().getTime();
-	while(t<=infra.htmlGetUnick.last_time)t++;
-	infra.htmlGetUnick.last_time=t;
+infra.htmlGetUnick = function () {
+	var t = new Date().getTime();
+	while (t <= infra.htmlGetUnick.last_time) t++;
+	infra.htmlGetUnick.last_time = t;
 	return t;
 };
-infra.htmlGetUnick.last_time=0;
+infra.htmlGetUnick.last_time = 0;
 
-infra.html.scriptautoexec=undefined;//Флаг выполняется ли скрипт сам при вставке html
-infra.html.styleautoexec=undefined;//Флга применяется ли <style> при вставке html
+infra.html.scriptautoexec = undefined;//Флаг выполняется ли скрипт сам при вставке html
+infra.html.styleautoexec = undefined;//Флга применяется ли <style> при вставке html
 
-if(typeof(document)=='object'){
-	document.writeold=document.write;
-	document.write=function(html){// нужно указывать document.write.div где нибудь в шаблоне и тогда фукнция сработает
-		var sdiv=document.write.div||'documentwrite';
-		var div=document.getElementById(sdiv);
-		if(div){
-			if(div.id=='documentwrite')div.id='';
-			infra.html(html,div);
-		}else{
-			if(infra.debug()){
-				debugger;
-				alert('Нужен document.write.div указать или создать ещё элемент с id '+sdiv);
-			}
-		}
-	}
-	document.write.div='documentwrite';
-} 
-
+window.View = infra.view = View
+export { View }
